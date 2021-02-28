d5d9d0fc76bdf186424be9ff6780ff68cf76b3f7
Savio Urzedo <saviobu@gmail.com>
2021-02-28 10:19:03 -0300

    Adicionado grafico

--- Atividade Coeficiente de Corelacao.py
+++ Atividade Coeficiente de Corelacao.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 df = pd.DataFrame({'Matematica':[5,8,7,10,6,7,9,3,8,2],'Estatistica':[6,9,8,10,5,7,8,4,6,2]})
 x = df['Matematica']
@@ -15,9 +16,9 @@ somay2 = (y**2).sum()
 soma2y = y.sum()*y.sum()
 
 r = (tam*somaxy - xy)/(math.sqrt( (tam*somax2 - soma2x)*(tam*somay2 - soma2y))) #Representa a qualidade dos dados/correlaçao ( reflexo com a realidade ), quanto mais proximo de 1 melhor
-b1 = (tam*somaxy - xy) / (tam*somax2 - soma2x) # Inclinação da reta
-b0 = (y.sum() - (b1*x.sum()))/tam # Interceptação da reta no eixo Y
-df ['Regressao Linear'] = (b0+(b1*x)).round(2)
+b1 = (tam*somaxy - xy) / (tam*somax2 - soma2x) 					# Inclinação da reta
+b0 = (y.sum() - (b1*x.sum()))/tam 						# Interceptação da reta no eixo Y
+df ['Regressao Linear'] = (b0+(b1*x)).round(2) 						#aplica a regressao nos casos da variavel dependente
 
 rel = None
 if r>0.9 and r<=1.0:
@@ -38,3 +39,7 @@ print('Resultados: B1= '+str(b1.round(2))+', B0= '+str(b0.round(2)))
 print ('A correlacao entre os dados é '+rel+': '+str(r.round(4)))
 print()
 print(df.to_string(index=False))
+
+plt.plot(df['Matematica'].values, df['Estatistica'].values,'ro')
+plt.plot(df['Regressao Linear'].values,df['Regressao Linear'].values)
+plt.show()
